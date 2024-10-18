6f612aa557a4a6d924f5afb13cb46e15e26eacb5
huhao7 <947416983@qq.com>
2024-10-18 10:20:29 +0800

    test

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+// import a secondary font called Lusitana
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
